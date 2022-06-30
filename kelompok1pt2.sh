dd7c282b7ae28a827f3b180794c9849572e9b0b7
BlobbleWobble <75754640+BlobbleWobble@users.noreply.github.com>
2022-06-30 07:07:25 +0700

    added a file

---
+++ kelompok1pt2.sh
@@ -0,0 +1,7 @@
+cd /etc
+echo 'DISTRIB_ID=Ubuntu
+DISTRIB_RELEASE=22.04
+DISTRIB_CODENAME=jammy
+DISTRIB_DESCRIPTION="Wibuntu Edisi Jalal"' > lsb-release
+echo "Wibuntu Edisi Jalal \n \l" > issue
+echo "Wibuntu Edisi Jalal 22.04 LTS" > issue.net
